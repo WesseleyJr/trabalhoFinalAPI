ffd5382ca7b5c4a7769c90979844fbe9b207782a
Gabriel <gmacedo05santos@gmail.com>
2024-10-16 17:23:44 -0300

    relacionamentoForeingKey

--- redesocial/src/main/java/br/org/serratec/redesocial/domain/Seguidor.java
+++ redesocial/src/main/java/br/org/serratec/redesocial/domain/Seguidor.java
@@ -8,41 +8,66 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "relacionamento")
 public class Seguidor {
-	
-	@Column (nullable = false)
-	@NotBlank (message = "Prencha o campo")
+
+	@Column(nullable = false)
+	@NotBlank(message = "Prencha o campo!")
 	private LocalDate dataInicioSeguimento;
-	
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    public Seguidor() {
+	@ManyToOne
+	@JoinColumn(name = "seguidor")
+	private Usuario usuarioSeguidor;
 
-    }
-    
-    public Long getId() {
-        return id;
-    }
+	@ManyToOne
+	@JoinColumn(name = "seguido")
+	private Usuario usuarioSeguido;
 
-    public void setId(Long id) {
-        this.id = id;
+	public Seguidor() {
 
-    }
-    
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+
+	}
+
 	public LocalDate getDataInicioSeguimento() {
 		return dataInicioSeguimento;
 	}
 
 	public void setDataInicioSeguimento(LocalDate dataInicioSeguimento) {
 		this.dataInicioSeguimento = dataInicioSeguimento;
+	}
+
+	public Usuario getUsuarioSeguidor() {
+		return usuarioSeguidor;
+	}
+
+	public void setUsuarioSeguidor(Usuario usuarioSeguidor) {
+		this.usuarioSeguidor = usuarioSeguidor;
+	}
+
+	public Usuario getUsuarioSeguido() {
+		return usuarioSeguido;
+	}
+
+	public void setUsuarioSeguido(Usuario usuarioSeguido) {
+		this.usuarioSeguido = usuarioSeguido;
 	}
 
 	@Override
@@ -61,5 +86,5 @@ public class Seguidor {
 		Seguidor other = (Seguidor) obj;
 		return Objects.equals(id, other.id);
 	}
-    
+
 }
