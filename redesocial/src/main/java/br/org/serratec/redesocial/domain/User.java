f42f0eb34d5d3e0fe1b7ac4c7572bcd3c449eac7
Julia Costa Freitas <jujuliacosta77frei@gmail.com>
2024-10-16 16:18:45 -0300

    Adicionando @ table

--- redesocial/src/main/java/br/org/serratec/redesocial/domain/User.java
+++ redesocial/src/main/java/br/org/serratec/redesocial/domain/User.java
@@ -8,14 +8,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 
 @Entity
+@Table(name = "User")
 public class User {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column
 	private Long id;
 
 	@Column(nullable = false)
