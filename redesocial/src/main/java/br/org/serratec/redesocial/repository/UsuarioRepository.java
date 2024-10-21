71aa15a814a552a02ceab8bdf162b42b1c97a0fb
Alexandre Conde <alexandreconde.adm@gmail.com>
2024-10-21 15:35:57 -0300

    atualização de querys repository

--- redesocial/src/main/java/br/org/serratec/redesocial/repository/UsuarioRepository.java
+++ redesocial/src/main/java/br/org/serratec/redesocial/repository/UsuarioRepository.java
@@ -8,14 +8,30 @@ import org.springframework.stereotype.Repository;
 
 import br.org.serratec.redesocial.domain.Usuario;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
+	// Busca de usuário por nome
 	@Query("SELECT u FROM Usuario u WHERE UPPER (u.nome) LIKE UPPER(CONCAT('%', :paramNome, '%'))")
-	Page<Usuario> buscarPorNome(String paramNome, Pageable pegeable);
-
-	@Query("SELECT u FROM Usuario u WHERE UPPER (u.email) LIKE UPPER(CONCAT('%', :paramEmail, '%'))")
-	Page<Usuario> buscarPorEmail(String paramEmail, Pageable pegeable);
+	Page<Usuario> buscarPorNome(String paramNome, Pageable pageable);
 
 	Usuario findByEmail(String email);
+
+	// Busca de usuário por data de nascimento
+	@Query("SELECT u FROM Usuario u WHERE u.dataNascimento BETWEEN :dataInicio AND :dataFim")
+	List<Usuario> buscarPorDataNascimentoEntre(LocalDate dataInicio, LocalDate dataFim);
+
+	// Busca de usuários por idade (com query nativa)
+	@Query(value = """
+			SELECT u.*, TIMESTAMPDIFF(YEAR, u.data_nascimento, CURDATE()) AS idade
+			FROM usuario u
+			HAVING idade = :idade
+			""",
+
+			nativeQuery = true)
+	List<Usuario> buscarPorIdade(Integer idade);
 }
+
